f4d21f22fa591643f77fdb3c50c0c6308ca37896
Rico Porter <rico.porter@capgemini.com>
2021-08-31 12:43:11 -0600

    init commit

---
+++ src/main/java/com/rico/SpringBootBankTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.rico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBankTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBankTransactionsApplication.class, args);
+	}
+
+}
